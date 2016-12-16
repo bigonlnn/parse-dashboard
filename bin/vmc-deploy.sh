2903918803effd030974af46d260c1deb375d89f
Van Munin Chhieng <v.m.chhieng@gmail.com>
2016-12-16 12:44:30 +0700

    testing

--- bin/vmc-deploy.sh
+++ bin/vmc-deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 npm install -g parse-dashboard
-parse-dashboard --appId trip-planner-on-heroku --masterKey master --serverURL "http://localhost:1337/parse"
+#parse-dashboard --appId trip-planner-on-heroku --masterKey master --serverURL "http://localhost:1337/parse" # local 
+#parse-dashboard --appId trip-planner-on-heroku --masterKey master --serverURL "https://tripplanner-101.herokuapp.com/parse" # remote 
+parse-dashboard --appId trip-planner-on-heroku --masterKey master --serverURL "https://sl-parse-server-dev.herokuapp.com/parse" 
